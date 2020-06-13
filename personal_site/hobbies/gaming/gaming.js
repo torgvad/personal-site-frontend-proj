a93fed54d9dd755149d1f98831949a0acefc449b
torgvad <vtorgashov44@gmail.com>
2020-06-12 23:26:41 -0500

    changed hobbies cards and made gaming page have images instead of vids

--- personal_site/hobbies/gaming/gaming.js
+++ personal_site/hobbies/gaming/gaming.js
@@ -1,17 +1,17 @@
 function get_game_text(game) {
 	switch (game) {
 		case "siege":
-			return "Rainbow Six Siege is a team based competitive tactical shooter. Similar to TF2 and Overwatch it lets you select an operator to play as, all of which have special abilities and weapons. This is one of my favorite games and one of my most played. The video is of a casual round with us on the defence.";
+			return "Rainbow Six Siege is a team based competitive tactical shooter. Similar to TF2 and Overwatch it lets you select an operator to play as, all of which have special abilities and weapons. This is one of my favorite games and one of my most played.";
 		case "overwatch": 
-			return "Overwatch is a hero team based shooter. This use to be one of my go to games and I racked up many hours. My main is Pharah. This is also the only game I ever played ranked in. It was only a few matches as I dropped the game fairly soon after. I played as healers as the role lock system was introduced around that time and the DPS queue was like 5-10 minutes. The video is me picking the game up after years of not playing so I was very rusty. I will eventaully come back to this game but I haven't been in the mood to play it in a long time.";
+			return "Overwatch is a hero team based shooter. This use to be one of my go to games and I racked up many hours. My main is Pharah. This is also the only game I ever played ranked in. It was only a few matches as I dropped the game fairly soon after. I played as healers as the role lock system was introduced around that time and the DPS queue was like 5-10 minutes. I will eventaully come back to this game but I haven't been in the mood to play it in a long time.";
 		case "payday2":
 			return "Payday 2 a co-op FPS which has you commit heists and bank robberies. This is fairly enjoyable game as I game gotten to a level where I have played every mission at least 2 dozen times and am I high level so the game is very laid back. I don't play really play six skulls especially after the difficulty rebalance made even 4 skulls a pretty good challenge at level 100. I always had the joke that I wanted to play one down four stores stealth and one day I said I wanted to do that in a lobby and they actually wanted to do it. After a few tries we actually pulled the mission off making it my only one down mission ever completed (this was back when 6 skulls was one down).";
 		case "rising_storm":
-			return "Rising Storm 2 a fairly hardcode FPS game in the Vietnam War setting. Most rifles will kill in a single round and the recoil is stout for firearms. The game has many technical flaws holding it back but when there's no issues it's extremely immersive and intense. My prefered role is a sniper cause I enjoy sitting 200 meters away and picking people off with a scoped M14. In this clip I joined an ongoing match so all the sniper roles were already taken. I was part of the North Vietnam military defending an objective.";
+			return "Rising Storm 2 a fairly hardcode FPS game in the Vietnam War setting. Most rifles will kill in a single round and the recoil is stout for firearms. The game has many technical flaws holding it back but when there's no issues it's extremely immersive and intense. My prefered role is a sniper cause I enjoy sitting 200 meters away and picking people off with a scoped M14.";
 		case "planetside": 
 			return "Planetside 2 is massive FPS game. The game involves attacking and defending provinces. There are four main maps each one being 8km by 8km and supporting up to 2000 people in total. There are three factions that fight each other over the provices of each map. The video is merely one battle of the 4 battles my faction was enganged in at the time. Other than the scale of the game one of the coller aspects is that it can feel like one continious battle. The transition we had from defending our province to going on the offensive was seamless. One minute we were pushing out attackers and the next I was in a vehicle on the way to help attack the province next door.";
 		case "h3vr": 
-			return "Hotdogs, Horseshoes, and Handgrenades is a VR firearm simulation. Every weapon in the game (which there are many with more being added regularly) is realistically simulated in function, appearance, and handling. Different ammunitions do different amounts of damage and have realistic bullet ballistics. This is easily my favorite VR game and has the most amount of hours of any VR game I own. The video is of me playing one of the main game modes called Take and Hold which involves taking objectives and defending them. The weapon spawn was set to random so every time I spent points on a gun I could get a .22 pistol or a mingun. The monitor only displays the left eye which is why you may not see everything.";
+			return "Hotdogs, Horseshoes, and Handgrenades is a VR firearm simulation. Every weapon in the game (which there are many with more being added regularly) is realistically simulated in function, appearance, and handling. Different ammunitions do different amounts of damage and have realistic bullet ballistics. This is easily my favorite VR game and has the most amount of hours of any VR game I own.";
 		case "zomboid": 
 			return "Project Zomboid is a zombie apocalypse simulation. The game has base building mechanics, a crafting system, many stats/skills, an in depth health system, and a Walking Dead stylezombie behaivor/AI. I have mods installed that greatly extend the cradting and construction in the game. The game map is based on the real county Kentuky with multple towns. This game is fairly relaxing as a lot of the time will be spent lurking around avoiding zombies, looting, constructing, and leveling certain stats. My typical strat is to find a farm house or some kind of large building right outside the city I spawn in. I then use the existing building to create a self sustaining base.";
 		case "rimworld": 
@@ -26,9 +26,9 @@ function get_game_text(game) {
 function change_video(selected_game) {
 	if (selected_game == "siege" || selected_game == "overwatch" || selected_game == "payday2" || selected_game == "rising_storm" || 
 	selected_game == "planetside" || selected_game == "h3vr" || selected_game == "zomboid" || selected_game == "rimworld" || selected_game == "minecraft") {
-		document.getElementById("vid_source").src = selected_game + ".mp4";	
-		document.getElementById("game_player").load();
-		document.getElementById("game_player").play();
+		document.getElementById("game_image").src = selected_game + ".jpg";	
+		//document.getElementById("game_player").load();
+		//document.getElementById("game_player").play();
 		document.getElementById("game_text").innerHTML = get_game_text(selected_game);
 	}
 }
